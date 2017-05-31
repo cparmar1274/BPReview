77d92858bb9274f0df0544ee035fbc7d878799d7
Parmar <PARMC23@TDBFG.COM>
2017-05-31 13:32:03 -0400

    changes

---
+++ src/main/java/org/tosca/BPReview/BpReviewApplication.java
@@ -0,0 +1,12 @@
+package org.tosca.BPReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpReviewApplication.class, args);
+	}
+}
